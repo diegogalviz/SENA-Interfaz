43c055a97c35981711e672a61551f03bc0d11456
diegogalviz <arqdiegogalvizv@gmail.com>
2022-07-27 20:37:00 -0500

    pantalla de creacion de cuentas

---
+++ js/app.js
@@ -0,0 +1,8 @@
+const form = document.querySelector('.form')
+const createAccount = document.querySelector('.create-account')
+
+createAccount.addEventListener('click', (e) => {
+    e.defaultPrevented
+    console.log('me diste click')
+
+})
